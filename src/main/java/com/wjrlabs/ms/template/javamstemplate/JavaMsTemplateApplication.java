f5076faec945500cab648c64d3307a3354660de9
Wagner Alves <wagner.junior2@ab-inbev.com>
2023-01-30 11:25:01 -0300

    Initial commit

---
+++ src/main/java/com/wjrlabs/ms/template/javamstemplate/JavaMsTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.wjrlabs.ms.template.javamstemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMsTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMsTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wjrlabs/ms/template/javamstemplate/JavaMsTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjrlabs.ms.template.javamstemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMsTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
